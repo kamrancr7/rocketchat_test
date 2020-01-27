d8069d19292310c74f88320305931b6821df841c
kamrancr7 <kamran.ali7293@gmail.com>
2020-01-27 21:59:35 +0500

    send message functionality.

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -8,21 +8,21 @@ class ChatScreen extends StatefulWidget {
   final userId;
   final roomId;
 
-  ChatScreen(this.authToken,this.userId,this.roomId);
+  ChatScreen(this.authToken, this.userId, this.roomId);
 
   @override
-  _ChatScreenState createState() => _ChatScreenState(authToken,userId,roomId);
+  _ChatScreenState createState() => _ChatScreenState(authToken, userId, roomId);
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   final _authToken;
   final _userId;
   final _roomId;
   ChatModel chatModel;
   bool isLoading = true;
+  final textController = TextEditingController();
 
-  _ChatScreenState(this._authToken,this._userId,this._roomId);
+  _ChatScreenState(this._authToken, this._userId, this._roomId);
 
   Future<String> getChatData() async {
     final response = await http.get(
@@ -36,8 +36,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
     chatModel = ChatModel.fromJson(json.decode(response.body));
     isLoading = false;
-    setState(() {
-    });
+    setState(() {});
   }
 
   @override
@@ -49,22 +48,58 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Chat Screen"),
-      ),
-      body: isLoading ? Center(child: CircularProgressIndicator()) :
-      ListView.builder(
-        reverse: true,
-        itemCount: chatModel.count
-      ,itemBuilder: (context,i) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(chatModel.messages[i].msg),
-          )
-        ],
-      )),
-    );
+        appBar: AppBar(
+          title: Text("Chat Screen"),
+        ),
+        body: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ListView.builder(
+                      reverse: true,
+                      itemCount: chatModel.count,
+                      itemBuilder: (context, i) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(chatModel.messages[i].msg),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          controller: textController,
+                        ),
+                      ),
+                      FlatButton(onPressed: () {
+                        sendMessage(_authToken,_userId,_roomId,textController.text);
+                      }, child: Text("Send"))
+                    ],
+                  )
+                ],
+              ));
+  }
+
+  Future<void> sendMessage(authToken, userId, roomId, msg) async {
+    String url = "https://iamflexi.com/iamflexi/api/v1/chat.sendMessage";
+    var body = json.encode({"message":
+    {
+      "rid": roomId,
+      "msg": msg
+    }});
+    final response = await http.post(Uri.encodeFull(url), //use your api there
+        body: body,
+        headers: {
+          "X-Auth-Token": authToken,
+          "X-User-Id": userId,
+          "Content-Type": "application/json"
+        });
+    print(json.decode(response.body));
   }
 }
